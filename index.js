f9797e2a9c6e690dbb657365536fed0f9f5b42ee
TTatchell <tim.tatchell24@gmail.com>
2021-03-11 17:21:59 +1100

    upload

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 62;
+const num2 = 1;
+const num3 = 20;
+const num4 = 8;
+const num5 = [1,2,3,4,20];
 
-// Write your code here
+console.log(num5);
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10 +1);
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,20);
+
+console.log(max);
